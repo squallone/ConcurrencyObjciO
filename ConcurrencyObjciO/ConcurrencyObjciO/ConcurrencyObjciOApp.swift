3b0902fa632f7a755c2460e1604a4c94cae46442
Abdiel <asoto@etsy.com>
2024-12-07 22:47:35 -0600

    Initial commit

---
+++ ConcurrencyObjciO/ConcurrencyObjciO/ConcurrencyObjciOApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConcurrencyObjciOApp.swift
+//  ConcurrencyObjciO
+//
+//  Created by Abdiel Soto on 07/12/24.
+//
+
+import SwiftUI
+
+@main
+struct ConcurrencyObjciOApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ConcurrencyObjciO/ConcurrencyObjciOTests/ConcurrencyObjciOTests.swift
@@ -0,0 +1,17 @@
+//
+//  ConcurrencyObjciOTests.swift
+//  ConcurrencyObjciOTests
+//
+//  Created by Abdiel Soto on 07/12/24.
+//
+
+import Testing
+@testable import ConcurrencyObjciO
+
+struct ConcurrencyObjciOTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
